a1f0c16e120f854da09b646bc9603f6e7b43aa48
NoahGigaByte <noahgboanerges@gmail.com>
2025-11-28 00:31:46 +0700

    added sidepanel and groups

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use eframe::{
     NativeOptions,
-    egui::{self, Color32, ColorImage, TextureHandle, Vec2, vec2},
+    egui::{self, Color32, ColorImage, RichText, TextureHandle, Vec2, vec2},
 };
 use image::{
     ImageBuffer
@@ -36,8 +36,14 @@ enum Pages {
 struct Ui {
     label_font_size: f32,
     heading_font_size: f32,
+
     watermark_texture: TextureHandle,
     watermark_size: Vec2,
+
+    sidepanel_width: f32,
+    sidepanel_on: bool,
+
+    padding: f32,
 }
 
 struct Droidium {
@@ -75,18 +81,45 @@ impl Droidium {
         Self { 
             current_page: Pages::Adb, 
             ui: Ui {
-                label_font_size: 14.0,
-                heading_font_size: 16.0,
+                label_font_size: 16.0,
+                heading_font_size: 20.0,
 
                 watermark_texture: tex,
                 watermark_size: size,
+
+                sidepanel_width: 40.0,
+                sidepanel_on: false,
+
+                padding: 10.0,
             } 
         }
     }
 }
 
 impl eframe::App for Droidium {
-    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::SidePanel::left("sidebar")
+            .resizable(false)
+            .exact_width(self.ui.sidepanel_width)
+            .show(ctx, |ui| {
+                ui.add_space(self.ui.padding);
+                ui.with_layout(egui::Layout::right_to_left(egui::Align::TOP), |ui| {
+                    // ui.add_space(self.ui.padding);
+                    let btn = ui.button(RichText::new("☰").color(Color32::WHITE).size(self.ui.heading_font_size));
+                    if btn.hovered() { let _ = egui::CursorIcon::PointingHand; }
+                    if btn.clicked() { 
+                        if self.ui.sidepanel_on { 
+                            self.ui.sidepanel_on = false;
+                            self.ui.sidepanel_width = 40.0; 
+                        }
+                        else { 
+                            self.ui.sidepanel_on = true;
+                            self.ui.sidepanel_width = 160.0; 
+                        }
+                    }
+                });
+
+            });
 
         egui::CentralPanel::default().show(ctx, |ui| {
             // wm part
@@ -94,19 +127,39 @@ impl eframe::App for Droidium {
             let painter = ui.painter_at(rect);
             
             let img_size = self.ui.watermark_size;
-            let center = rect.center();
+            let rectx = (1280.0 / 2.0) - (img_size.x / 2.0);
+            let recty = (600.0 / 2.0) - (img_size.y / 2.0);
 
-            let target_rect = egui::Rect::from_center_size(center, img_size);
+            let target_rect = egui::Rect::from_min_max(
+                egui::pos2(rectx, recty),
+                egui::pos2(rectx + self.ui.watermark_size.x, recty + self.ui.watermark_size.y)
+            );
 
             painter.image(
                 self.ui.watermark_texture.id(), 
-                target_rect, 
-                egui::Rect::from_min_max(egui::Pos2::new(0.0, 0.0), egui::Pos2::new(1.0, 1.0)), 
-                Color32::from_white_alpha(20),
+                target_rect,  // this one for the positioning in the panel
+                egui::Rect::from_min_max(egui::Pos2::new(0.0, 0.0), egui::Pos2::new(1.0, 1.0)), // this one for the image
+                Color32::from_white_alpha(10),
             );
             // end wm part
 
+            ui.horizontal(|ui| {
+                ui.set_height(600.0 - self.ui.padding * 2.0);
+                ui.group(|ui| {
+                    ui.set_width(1280.0 / 2.0 - 40.0);
 
+                    ui.label("kotnol");
+                    ui.label("kotnol");
+                    ui.label("kotnol");
+                });
+                ui.group(|ui| {
+                    ui.set_width(ui.available_width() - 4.0);
+                    
+                    ui.label("kotnol");
+                    ui.label("kotnol");
+                    ui.label("kotnol");
+                });
+            });
 
         });
     }
